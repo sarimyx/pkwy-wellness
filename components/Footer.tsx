ff09435976ae821827e4f1672513dd00e7785208
Sarim <sarimyx@proton.me>
2025-08-21 23:13:52 -0600

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,19 +4,18 @@ import Image from 'next/image';
 export function Footer() {
   return (
     <footer className={`bg-black text-white`}>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-left py-8">
-        <h3 className="text-2xl mb-4 font-light">PKWY Wellness</h3>
-        <p className="text-gray-400 max-w-2xl font-light">
-          Transform your body and mind with expert Pilates instruction. Private sessions, group classes, and corporate wellness programs. Serving Pittsburgh and surrounding areas.
-        </p>
-        <div className="flex justify-end">
+      <div className="mx-auto px-4 text-left py-4">
+        <section className='flex justify-between items-center text-xs text-gray-500'>
           <Image
             src="/branding-kit/PKWY-logo.png"
             alt="PKWY Wellness Logo"
             width={50}
             height={50}
           />
-        </div>
+          <p>
+            © 2025 PKWY Wellness
+          </p>
+        </section>
       </div>
     </footer>
   );
