d882a517a759ec464754af40efdeb82cf71e7e06
Sarim <sarimyx@proton.me>
2025-08-30 21:22:52 -0600

    refactor + ui

--- config/identity.ts
+++ config/identity.ts
@@ -1,11 +1,11 @@
 export const Identity = {
     name: "PKWY Pilates",
     companyName: "PKWY Wellness",
-    url: "https://pkwypilates.com",
+    url: "https://pkwywellness.com",
     description: "Transforming lives through the power of Pilates. Based in Pittsburgh, PA.",
     logo: "/branding/logo.png",
     email: "contact@pkwypilates.com",
     social: { 
-        instagram: "https://www.instagram.com/pkwypilates/"
+        instagram: "https://www.instagram.com/pkwypilates"
     }
 }
