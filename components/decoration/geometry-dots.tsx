fe053da59a1b44acb363ecf3fd1004c967efeba7
Sarim <sarimyx@proton.me>
2025-09-01 21:43:37 -0600

    add components

---
+++ components/decoration/geometry-dots.tsx
@@ -0,0 +1,6 @@
+export function GeometryDots() {
+    return (
+        <div className="absolute -z-10 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+    )
+}
+
