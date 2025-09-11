2f9d9db75c2ef8ca398430e2d921533613b61c3d
Sarim <sarimyx@proton.me>
2025-09-11 17:29:58 -0600

    update street

--- config/identity.ts
+++ config/identity.ts
@@ -12,11 +12,11 @@ export const Identity = {
     },
     address: { 
         name: "Wilkinsburg Community Development Corporation",
-        street: "729 Wood St",
+        street: "727 Wood St",
         city: "Pittsburgh",
         state: "PA",
         zip: "15221",
-        complete: "Wilkinsburg Community Development Corporation, 729 Wood St, Pittsburgh, PA 15221",
+        complete: "Wilkinsburg Community Development Corporation, 727 Wood St, Pittsburgh, PA 15221",
         googleMaps: "https://maps.app.goo.gl/PKjiwpu5dwBfbwxU7",
         appleMaps: "https://maps.apple.com/place?place-id=IBCDEB34EFC295E6B&address=1001+Wood+St%2C+Pittsburgh%2C+PA++15221%2C+United+States&coordinate=40.44396%2C-79.884904"
     }
